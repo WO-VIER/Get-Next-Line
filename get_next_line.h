0bc4aecfe024ddfa8b8b6edf63bc4bbf62b1f32e
Virgile Wautier <117287561+WO-VIER@users.noreply.github.com>
2024-12-02 16:11:04 +0100

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -3,23 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vwautier <vwautier@student.s19.be>         +#+  +:+       +#+        */
+/*   By: vwautier <vwautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 16:36:39 by vwautier          #+#    #+#             */
-/*   Updated: 2024/11/07 15:55:17 by vwautier         ###   ########.fr       */
+/*   Updated: 2024/12/02 12:49:42 by vwautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h> 
-# include <stdio.h>
 
 # ifndef BUFFER_SIZE
-# 	define BUFFER_SIZE 5
+#  define BUFFER_SIZE 5
 # endif
 
 typedef struct s_list
@@ -27,21 +25,15 @@ typedef struct s_list
 	void			*content;
 	struct s_list	*next;
 }	t_list;
-void	ft_lstclear(t_list **lst);
-void read_save(t_list *save, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-char *ft_strdup(const char *s);
-size_t ft_strlen(const char *s);
-size_t size_content(t_list *list);
-void populatestr(char *str, t_list *list);
-void ft_putchar_fd(int fd, char c);
-void ft_putstr_fd(int fd, char *str);
-int have_new_line(t_list *list);
-int have_new_line_last(t_list *list);
-t_list *ft_get_last(t_list *list);
-char *extract_line(t_list *list);
-char *get_next_line(int fd);
+void	ft_lstclear(t_list **lst);
+char	*ft_strdup(const char *s);
+size_t	size_content(t_list *list);
+char	*get_next_line(int fd);
+void	read_save(t_list **save, char *buffer, int fd);
+void	extract_line(t_list **list, char **ligne);
+void	clean_save(t_list **save);
+int		have_new_line_last(t_list *list);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vwautier <vwautier@student.s19.be>         +#+  +:+       +#+        */
+/*   By: vwautier <vwautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 16:36:36 by vwautier          #+#    #+#             */
-/*   Updated: 2024/11/07 16:50:15 by vwautier         ###   ########.fr       */
+/*   Updated: 2024/12/02 12:42:11 by vwautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,6 @@ t_list	*ft_lstnew(void *content)
 	node->content = content;
 	node->next = NULL;
 	return (node);
-}
-
-void	ft_lstadd_front(t_list **lst, t_list *new)
-{
-	if (lst != NULL && new != NULL)
-	{
-		new->next = *lst;
-		*lst = new;
-	}
 }
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
@@ -68,135 +59,50 @@ void	ft_lstclear(t_list **lst)
 	*lst = NULL;
 }
 
-char *ft_strdup(const char *s)
+char	*ft_strdup(const char *s)
 {
-	char *str;
-	size_t len;
+	char	*str;
+	size_t	len;
+
 	if (!s)
-		return(NULL);
-	len = ft_strlen(s);
+		return (NULL);
+	len = 0;
+	while (s[len] != '\0')
+		len++;
 	str = malloc(sizeof(char) * (len + 1));
-	if(!str)
+	if (!str)
 		return (NULL);
 	str[len] = '\0';
 	len = 0;
-	while(*s)
+	while (*s)
 		str[len++] = *s++;
-	return (str); 
+	return (str);
 }
 
-size_t ft_strlen(const char *s)
+size_t	size_content(t_list *list)
 {
-	size_t i;
-	if(!s)
-		return (0);
-	i = 0;
-	while(s[i] != '\0' && s[i] != '\n') 
-		i++;
-	return(i);
-}
+	size_t	size;
+	t_list	*current;
+	char	*content;
 
-size_t size_content(t_list *list)
-{
-	size_t size;
-	t_list *current;
-
-	if(!list)
+	if (!list)
 		return (0);
 	size = 0;
 	current = list;
-
 	while (current)
 	{
-		size = size + ft_strlen((char*)current->content);
+		content = (char *)current->content;
+		while (*content != '\0' && *content != '\n')
+		{
+			size++;
+			content++;
+		}
+		if (*content == '\n')
+		{
+			size++;
+			break ;
+		}
 		current = current->next;
 	}
 	return (size);
-}
-
-int have_new_line_last(t_list *list)
-{
-	char *content;
-	t_list *last;
-
-	if (!list)
-		return(0);
-
-	last = ft_get_last(list);
-
-	if(last->next == NULL)
-	{
-		content = (char *)last->content;
-		while(*content != '\0')
-		{
-			if(*content == '\n')
-				return (1);
-			*content++;
-		}
-	}
-	return (0);
-}
-
-t_list *ft_get_last(t_list *list)
-{
-	t_list *current;
-
-	current = list;
-	while (current && current->next)
-		current = current->next;
-	return (current);
-}
-
-void populatestr(char *str, t_list *list)
-{
-	t_list 	*current;
-	char 	*content;
-
-	if(!str || !list)
-		return ;
-	current = list;
-	content = (char *)current->content;
-	while (current != NULL)
-	{
-		while(*content != '\0')
-		{
-			*str = *content++;
-			*str++;
-		}
-		current = current->next;
-		if(current != NULL)
-			content = (char *)current->content;
-	}
-}
-
-void ft_putchar_fd(int fd, char c)
-{
-	write(fd,&c,1);
-}
-
-void ft_putstr_fd(int fd, char *str)
-{
-	while(*str)
-	{
-		ft_putchar_fd(fd,*str++);
-	}
-}
-
-int have_new_line(t_list *list)
-{
-	char *content;
-	if (!list)
-		return(0);
-	while (list != NULL)
-	{
-		content = (char *)list->content;
-		while(*content != '\0')
-		{
-			if (*content == '\n')
-				return (1);
-			*content++;
-		}
-		list = list->next;
-	}
-	return (0);
 }
